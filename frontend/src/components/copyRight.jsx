5f3f7d23daef85c041732c8eceaf8dcb74f78bc9
Xiaoheng Hong <xiaoheng.hong@student.unsw.edu.au>
2021-04-06 20:22:15 +1000

    create components folder

---
+++ frontend/src/components/copyRight.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+/*
+Material UI templates
+https://material-ui.com/getting-started/templates/
+*/
+const Copyright = () => {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © COMP6080 BigBrain by Zhang Yu & Hong Xiaoheng'}
+        </Typography>
+    );
+}
+
+export default Copyright;
